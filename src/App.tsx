f65aab171b6a7b3d320e0aec4d825ba23025e483
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-03-15 13:36:47 -0300

    [add] button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 import './App.css'
 import RandomFox from './components/RandomFox'
 
@@ -12,18 +12,27 @@ function App() {
     url: string
   }
 
-  const [images, setImages] = useState<ImageItems[]>([
-    {id: generateId(), url: `https://randomfox.ca/images/${randomNum()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${randomNum()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${randomNum()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${randomNum()}.jpg`},
-    {id: generateId(), url: `https://randomfox.ca/images/${randomNum()}.jpg`},
-  ])
+  const [images, setImages] = useState<ImageItems[]>([])
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (e) => {
+    
+    e.preventDefault()
+
+    const newFox = {
+        id: generateId(), 
+        url: `https://randomfox.ca/images/${randomNum()}.jpg`
+      }
+      setImages([
+        newFox,
+        ...images
+      ])
+  }
 
   return (
     <div className='app'>
       <h1 className='text-3xl font-bold underline'>Hola mundovich</h1>
       <h4>Random Foxes Generator</h4>
+      <button onClick={addNewFox} className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Add fox</button>
       <div>
         {images.map(({ id, url }) => {
           return (
