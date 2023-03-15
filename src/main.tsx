87964b0278713ab6272220fe6989860d9bd815ae
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-03-15 15:32:54 -0300

    [add] lazy loading images

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SkeletonTheme } from 'react-loading-skeleton'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SkeletonTheme baseColor="#202020" highlightColor="#999">
-      <App />
-    </SkeletonTheme>
+    <App />
   </React.StrictMode>,
 )
